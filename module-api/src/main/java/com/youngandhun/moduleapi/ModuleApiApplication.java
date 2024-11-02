166839c90bc86fcc69062e1547cae0396ef766dd
chaeyoungeee <chaeinge@gmail.com>
2024-11-02 23:07:02 +0900

    Fix: cors 에러 해결

--- module-api/src/main/java/com/youngandhun/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/youngandhun/moduleapi/ModuleApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.youngandhun.modulecore", "com.youngandhun.modulecommon", "com.youngandhun.moduleapi"})
 @EntityScan(basePackages = "com.youngandhun.modulecore")
 @EnableJpaRepositories(basePackages = "com.youngandhun.modulecore")
 @EnableJpaAuditing
